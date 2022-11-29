e79facccffd08b1f3084c82ad9d0093809fd4755
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-11-29 21:52:35 +0530

    Init

---
+++ src/Component/FunctionalClick.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function FunctionalClick() {
+
+    function clickHandler(){
+        console.log("button clicked")
+    }
+
+  return (
+    
+
+    <div>
+      <button onClick={clickHandler}>Click</button>
+    </div>
+  );
+}
+
+export default FunctionalClick;
